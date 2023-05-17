66292b0d77d13bac255a090fbaaae695a7ac031a
Bruno Franco <brunoantunesfranco@gmail.com>
2023-05-17 18:07:15 +0200

    exit mb

--- srcs/builtins/exit.c
+++ srcs/builtins/exit.c
@@ -6,30 +6,34 @@
 /*   By: bfranco <bfranco@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/05/09 16:21:10 by bfranco       #+#    #+#                 */
-/*   Updated: 2023/05/12 10:46:27 by bfranco       ########   odam.nl         */
+/*   Updated: 2023/05/17 18:06:49 by bfranco       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+#include <limits.h>
 
 static int	atoi_exit(char *str)
 {
-	long long	nb;
-	int			i;
+	unsigned char	nb;
+	int				i;
+	long int		nb2;
 
 	i = 0;
 	nb = 0;
-	if (!ft_strlen(str) || str[0] == '-')
+	nb2 = 0;
+	if (!str || !ft_strlen(str))
 		return (-1);
-	if (str[0] == '+')
+	if (str[0] == '+' || str[0] == '-')
 		i++;
 	while (str[i])
 	{
 		if (str[i] >= '0' && str[i] <= '9')
 		{
 			nb = nb * 10 + (str[i] - '0');
+			nb2 = nb2 * 10 + (str[i] - '0');
 			i++;
-			if (nb < 0 || nb > INT_MAX)
+			if ((int)nb2 < INT_MIN || (int)nb2 > INT_MAX)
 				return (-1);
 		}
 		else
@@ -38,14 +42,17 @@ static int	atoi_exit(char *str)
 	return ((int)(nb));
 }
 
+
 int	mini_exit(t_gen *gen, t_cmd *cmd)
 {
 	int	exit_code;
 
 	exit_code = 0;
+	if (write(2, "exit\n", 6) == -1)
+		return (err_msg(cmd->cmd[0], "write error"), -1)
 	if (ft_arrlen(cmd->cmd) > 2)
 	{
-		built_err_msg(cmd->cmd[0], NULL, "minishell: exit: too many arguments");
+		built_err_msg(cmd->cmd[0], NULL, "too many arguments\n");
 		exit(1);
 	}
 	if (cmd->cmd[1])
@@ -54,8 +61,7 @@ int	mini_exit(t_gen *gen, t_cmd *cmd)
 		if (exit_code < 0 || exit_code > 255 )
 		else
 		{
-			//error msg
-			errno = 255;
+			built_err_msg(cmd->cmd[0], NULL, "numeric argument required\n");
 			exit(255);
 		}
 	}
