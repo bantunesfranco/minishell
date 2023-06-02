f9f41e313d0cd814e0e11755e7cac3b49fd74404
Jan Molenaar <molenaarjana@gmail.com>
2023-06-02 19:03:30 +0200

    Fri Jun  2 2023 some expansion stuff

--- srcs/input_parser/expansion/expansion.c
+++ srcs/input_parser/expansion/expansion.c
@@ -6,7 +6,7 @@
 /*   By: jmolenaa <jmolenaa@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/06/01 14:51:40 by jmolenaa      #+#    #+#                 */
-/*   Updated: 2023/06/02 13:43:26 by jmolenaa      ########   odam.nl         */
+/*   Updated: 2023/06/02 14:48:32 by jmolenaa      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,9 +24,16 @@ size_t	skip_quotes(char *line, size_t i)
 	return (i);
 }
 
-void	expand_variable(char *word, size_t i)
+size_t	skip_to_end(char *word, size_t start_of_var)
 {
-	
+	while (*(word + start_of_var) != '\0')
+}
+
+size_t	expand_variable(char *word, size_t start_of_var, t_gen *gen)
+{
+	size_t	end_of_var;
+
+	end_of_var = skip_to_end(word, start_of_var);
 }
 
 void	expand_environment_vars(char *word, t_gen *gen)
@@ -39,7 +46,7 @@ void	expand_environment_vars(char *word, t_gen *gen)
 	while (*(word + i) != '\0')
 	{
 		if (*(word + i) == '$')
-			expand_variable(word, i);
+			i = expand_variable(word, i, gen);
 		if (*(word + i) == '\'')
 			i = skip_quotes(word, i);
 		i++;
