8907f3f399a8cb0f1bc793663d7c4722297afc67
Bruno Franco <94724432+bantunesfranco@users.noreply.github.com>
2023-06-03 10:34:42 +0000

    test

--- incs/executor.h
+++ incs/executor.h
@@ -6,7 +6,7 @@
 /*   By: codespace <codespace@student.codam.nl>       +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/05/22 12:31:23 by codespace     #+#    #+#                 */
-/*   Updated: 2023/06/02 10:25:56 by bfranco       ########   odam.nl         */
+/*   Updated: 2023/06/03 10:34:27 by codespace     ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,5 +25,6 @@ int		pipe_and_fork(t_cmd *cmd, int *p);
 void	close_pipes(t_cmd *cmd, int *p, int pipe_rd);
 void	exec_cmd(t_gen *gen, t_cmd *cmd, int *p, int pipe_rd);
 void	executor(t_gen *gen, t_pipeline *pipeline);
+
 
 #endif
